97317f4f4ed0e4017929ecaf4561882b86086be6
sanskargangwar <145826905+sanskargangwar@users.noreply.github.com>
2026-06-23 16:56:41 +0530

    Update getUser.js

--- Backend/routes/getUser.js
+++ Backend/routes/getUser.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/User.js";
-import { authMiddleware } from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
